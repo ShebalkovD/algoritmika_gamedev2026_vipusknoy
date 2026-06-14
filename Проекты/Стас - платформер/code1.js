f8de25b9e6805c095e84e464da52b02f4d9c8340
DShebalkov <shebalkovdenis.wk@gmail.com>
2026-06-14 10:50:44 +0300

    feat: Добавить проекты платформеры для Стаса и Лёни

---
+++ Проекты/Стас - платформер/code1.js
@@ -0,0 +1,21 @@
+gdjs._1091_1088_1086_1074_1077_1085_1100_322Code = {};
+gdjs._1091_1088_1086_1074_1077_1085_1100_322Code.localVariables = [];
+gdjs._1091_1088_1086_1074_1077_1085_1100_322Code.idToCallbackMap = new Map();
+
+
+gdjs._1091_1088_1086_1074_1077_1085_1100_322Code.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs._1091_1088_1086_1074_1077_1085_1100_322Code.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs._1091_1088_1086_1074_1077_1085_1100_322Code.eventsList0(runtimeScene);
+
+
+return;
+
+}
+
+gdjs['_1091_1088_1086_1074_1077_1085_1100_322Code'] = gdjs._1091_1088_1086_1074_1077_1085_1100_322Code;
